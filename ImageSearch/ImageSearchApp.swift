eb6559f3fe435a5e6786d1f3dbca691cc07045a7
takin park <takin park>
2026-03-16 15:42:00 +0900

    프로젝트 생성

---
+++ ImageSearch/ImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchApp.swift
+//  ImageSearch
+//
+//  Created by 박탁인 on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
